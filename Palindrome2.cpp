ac0955f24b755abc6f8e8c52805e5fcf2d53496e
Andrew Kyumin Cho <akcho@u.northwestern.edu>
2020-05-08 11:09:58 -0700

    created Palindrome2.cpp

--- Palindrome2.cpp
+++ Palindrome2.cpp
@@ -1,4 +1,39 @@
-//
-// Created by Administrator on 5/8/2020.
-//
+#include <iostream>
+using namespace std;
 
+class Node {
+public:
+    int data;
+    Node* next;
+};
+
+void PrintList(Node* n) {
+    while (n != nullptr) {
+        cout << n->data << " " ;
+        n = n->next;
+    }
+}
+
+int main() {
+    Node* head = NULL;
+    Node* second = NULL;
+    Node* third = NULL;
+
+    // allocate 3 nodes in the heap
+    head = new Node();
+    second = new Node();
+    third = new Node();
+
+    head->data = 1; // assign data in first node
+    head->next = second; // Link first node with second
+
+    second->data = 2; // assign data to second node
+    second->next = third;
+
+    third->data = 3; // assign data to third node
+    third->next = NULL;
+
+    PrintList(head);
+
+    return 0;
+}
