133f06450154aeb05eb670fa65e3dde007636499
Andrew Cho <andrew@augmentedrealitycapture.com>
2020-05-14 17:33:51 -0700

    completed LoopDetection

--- LoopDetection.cpp
+++ LoopDetection.cpp
@@ -24,17 +24,37 @@ Node* FindBeginning(Node* head) {
         fast = fast->next->next;
         if (slow == fast) break;    // Collision
     }
+
+    /* Error check - no meeting point, and therefore no loop */
+    if (fast == nullptr || fast->next == nullptr) return nullptr;
+
+    /* Move slow to Head. Keep fast at Meeting Point. Each are k steps from the Loop Start. If they move at the same
+     * pace, they must meet at Loop Start. */
+
+    slow = head;
+    while (slow != fast) {
+        slow = slow->next;
+        fast = fast->next;
+    }
+    /* Both now point to the start of the loop. */
+    return slow;
 }
 
 int main() {
     Node* head = NULL;
     Node* second = NULL;
     Node* third = NULL;
+    Node* fourth = NULL;
+    Node* fifth = NULL;
+    Node* sixth = NULL;
 
     // allocate 3 nodes in the heap
     head = new Node();
     second = new Node();
     third = new Node();
+    fourth = new Node();
+    fifth = new Node();
+    sixth = new Node();
 
     head->data = 1; // assign data in first node
     head->next = second; // Link first node with second
@@ -43,9 +63,18 @@ int main() {
     second->next = third;
 
     third->data = 3; // assign data to third node
-    third->next = NULL;
+    third->next = fourth;
 
-    PrintList(head);
+    fourth->data = 4; // assign data to third node
+    fourth->next = fifth;
+
+    fifth->data = 5; // assign data to third node
+    fifth->next = sixth;
+
+    sixth->data = 6; // assign data to third node
+    sixth->next = fourth;
+
+    cout << FindBeginning(head)->data;
 
     return 0;
 }
