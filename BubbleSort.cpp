74f5c59cd65caf350e2249f500f5583ec4a21f0e
Andrew Kyumin Cho <akcho@u.northwestern.edu>
2020-06-09 14:55:09 -0700

    added BubbleSort.cpp

--- BubbleSort.cpp
+++ BubbleSort.cpp
@@ -1,4 +1,29 @@
-//
-// Created by Administrator on 6/5/2020.
-//
+#include <iostream>
+#include <vector>
+#include <string>
+using namespace std;
 
+void BubbleSort(string s) {
+    bool is_swapped = true;
+
+    while (is_swapped) {
+        is_swapped = false;
+        for (int i = 0; i < s.size(); i++) {
+            if (s[i + 1] < s[i] && s[i+1] > 0) {
+                char to_swap = s[i];
+                s[i] = s[i+1];
+                s[i + 1] = to_swap;
+                is_swapped = true;
+            }
+        }
+    }
+    cout << s;
+}
+
+
+int main() {
+    string s = "hello";
+
+    BubbleSort(s);
+    return 0;
+}
